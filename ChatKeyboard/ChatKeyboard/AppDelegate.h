372919d7b5900ada8813341d3f219efd8792e9b5
haizhiwen <wangyajun@wangyajundeMacBook-Pro.local>
2017-04-29 15:38:04 +0800

    键盘
    上传键盘

---
+++ ChatKeyboard/ChatKeyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChatKeyboard
+//
+//  Created by 王亚军 on 2017/4/29.
+//  Copyright © 2017年 王亚军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChatKeyboard/ChatKeyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChatKeyboard
+//
+//  Created by 王亚军 on 2017/4/29.
+//  Copyright © 2017年 王亚军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
